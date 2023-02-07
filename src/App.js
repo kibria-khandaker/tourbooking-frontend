f370c40570b4344845819669e4d91c7e728bc007
Golam Kibria <kibriakhandaker@gmail.com>
2023-02-08 04:33:37 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return <Layout />;
+}
+
+export default App;
+
+// 'JetBrains Mono', Consolas, 'Courier New', monospace
+// 'JetBrains Mono', Monaco, Consolas, 'Courier New', monospace 
