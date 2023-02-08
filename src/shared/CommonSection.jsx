8632271f7c30ac845887b5dbb99009283b700aa7
Golam Kibria <kibriakhandaker@gmail.com>
2023-02-09 01:25:22 +0600

    CommonSection start

---
+++ src/shared/CommonSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./common-section.css";
+
+import { Col, Container, Row } from "reactstrap";
+const CommonSection = () => {
+  return <>
+
+
+
+  </>;
+};
+
+export default CommonSection;
