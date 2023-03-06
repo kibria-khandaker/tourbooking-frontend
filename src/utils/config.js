6b6429bcb59037477199cf294c07867ca36ba2a4
Golam Kibria <kibriakhandaker@gmail.com>
2023-03-06 21:49:00 +0600

    update text

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,9 +4,13 @@
 // in render.com host
 export const BASE_URL = 'https://tour-book01-api.onrender.com/api/v1'
 
+
+
+
+
+
 // in railway.app host
 // export const BASE_URL = 'https://tourbooking-backend-production.up.railway.app/api/v1'
-
 
 // in https://app.cyclic.sh/ host
 // export const BASE_URL = 'https://drab-erin-wildebeest-wear.cyclic.app/api/v1'
