b0557ce7734e487b3aa03356e5b6848c66d6b9e1
Golam Kibria <kibriakhandaker@gmail.com>
2023-04-26 01:35:49 +0600

    Link change

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 // export const BASE_URL = 'http://localhost:4000/api/v1'
 
 // in render.com host
-export const BASE_URL = 'https://tour-book01-api.onrender.com/api/v1'
+// export const BASE_URL = 'https://tour-book01-api.onrender.com/api/v1'
 
 
 
@@ -10,7 +10,7 @@ export const BASE_URL = 'https://tour-book01-api.onrender.com/api/v1'
 
 
 // in railway.app host
-// export const BASE_URL = 'https://tourbooking-backend-production.up.railway.app/api/v1'
+export const BASE_URL = 'https://tourbooking-backend-production.up.railway.app/api/v1'
 
 // in https://app.cyclic.sh/ host
 // export const BASE_URL = 'https://drab-erin-wildebeest-wear.cyclic.app/api/v1'
